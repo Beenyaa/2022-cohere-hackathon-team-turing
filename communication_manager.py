9784950ff40a64806b05fd62ba5012dd4cbc2bbc
gasparyanartur <gasparyanartur99@gmail.com>
2022-08-20 15:33:49 +0200

    Add interactive cli

--- communication_manager.py
+++ communication_manager.py
@@ -3,28 +3,9 @@ class CommunicationManager:
         print(f"{sender}: {msg}")
 
     def receive_message(self) -> tuple[str, str]:
-        """
-        message [name] [msg]
-        choices [name] [choice1]|[choice2]|...
-        """
         raw_data = input()
         data = raw_data.split(' ')
-        if len(data) < 3:
+        if len(data) < 2:
             raise ValueError(f"Failed to parse sender and message from {raw_data}")
 
-        if data[0] == 'message':
-            return data[0], data[1], ' '.join(data[2:])
-
-        elif data[0] == 'choices':
-            joined_data = ' '.join(data[2:])
-            return data[0], data[1], joined_data.split('|')
-        
-        raise ValueError(f"Failed to parse sender and message from {raw_data}")
-
-    def receive_messages(self) -> tuple[str, list[str]]:
-        raw_data = input()
-        data = raw_data.split('|')
-        if len(data) >= 2:
-            return data[0], data[1:]
-        
-        raise ValueError(f"Failed to parse sender and message from {raw_data}")
+        return data[0], ' '.join(data[1:])
--- customer_support.py
+++ customer_support.py
@@ -1,5 +1,6 @@
 from enum import Enum, auto
 from communication_manager import CommunicationManager
+from ai_manager import AIManager
 
 
 class Action(Enum):
@@ -16,40 +17,41 @@ action_commands = {
 
 
 class CustomerSupportChat:
-    def __init__(self, name: str, comm_manager: CommunicationManager) -> None:
+    def __init__(self, name: str, comm_manager: CommunicationManager, ai_manager: AIManager) -> None:
         self.name = name
         self._comm_manager = comm_manager
+        self._ai_manager = ai_manager
 
     def run(self):
         while True:
             print("Waiting for message...")
-            msg_type, sender, msg = comm_manager.receive_message()
+            sender, msg = comm_manager.receive_message()
 
-            if msg_type == 'message':
-                self.recieve_message(sender, msg)
-                self.send_message(input(f"{self.name}: "))
-            
-            elif msg_type == 'choices':
-                self.receive_choices(sender, msg)
-                msg_index = c.select_choice(msg)
+            self.display_message(sender, msg)
+            answers = self._ai_manager.answer_message(msg)
+
+            self.display_answers(answers)
+
+            while True:
+                select_index = self.choose_message(answers)
                 action = self.choose_action()
-                c.handle_action(msg, msg_index, action)
-
-
+                terminate = self.handle_action(answers, select_index, action)
+                if terminate:
+                    break
 
 
     def send_message(self, msg: str) -> None:
         self._comm_manager.send_message(self.name, msg)
 
-    def recieve_message(self, sender, msg: str) -> None:
+    def display_message(self, sender, msg: str) -> None:
         print(f"{sender}: {msg}")
 
-    def receive_choices(self, sender, msgs: list[str]) -> None:
-        print(f"{sender}:")
+    def display_answers(self, msgs: list[str]) -> None:
+        print("Answers:")
         for i, m in enumerate(msgs):
             print(f"\t{i+1}: {m}")
 
-    def select_choice(self, msgs: list[str]) -> int:
+    def choose_message(self, msgs: list[str]) -> int:
         index = input()
         if not index.isdigit():
             raise ValueError(f"Could not find message with id: '{index}'")
@@ -65,7 +67,7 @@ class CustomerSupportChat:
         action = input().strip().lower()
         return action_commands[action]
 
-    def handle_action(self, msgs: str, index: int, action: Action):
+    def handle_action(self, msgs: str, index: int, action: Action) -> bool:
         match action:
             case Action.Edit:
                 print(f"Editing ({msgs[index]}): ", end='')
@@ -73,31 +75,18 @@ class CustomerSupportChat:
                 msgs[index] = new_msg
                 for i, m in enumerate(msgs):
                     print(f"\t{i+1}: {m}")
+                return False
 
             case Action.Send:
-                print("SENDING")
-                raise NotImplementedError
+                self.send_message(msgs[index])
+                return True
 
             case Action.Unselect:
-                print("UNSELECT")
-                raise NotImplementedError
+                return True
 
 
 if __name__ == "__main__":
     comm_manager = CommunicationManager()
-    c = CustomerSupportChat("Customer Support", comm_manager)
-    """
-    c.send_message("Hello")
-    #sender, msg = comm_manager.receive_message()
-    #c.recieve_message(sender, msg)
-
-    sender, msgs = comm_manager.receive_messages()
-    c.receive_choices(sender, msgs)
-
-    msg_index = c.select_choice(msgs)
-    print(msgs[msg_index])
-
-    action = c.choose_action()
-    c.handle_action(msgs, msg_index, action)
-    """
+    ai_manager = AIManager()
+    c = CustomerSupportChat("Customer Support", comm_manager, ai_manager)
     c.run()
